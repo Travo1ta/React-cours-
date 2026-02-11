cab964d073146ef5137c12adaefc6b5bb3166377
Travo1ta <vladimirkobec@gmail.com>
2026-02-11 15:54:25 +0300

    Price recalculation

--- src/components/ProductPage/ProductPage.jsx
+++ src/components/ProductPage/ProductPage.jsx
@@ -6,81 +6,89 @@ import Description from '../Description/Description';
 import Comments from '../Comments/Comments';
 import Tabs from '../Tabs/Tabs';
 import {
-   StyledProductPage,
-   Header,
-   ProductWrapper,
-   ProductInfo,
-   ProductInfoLine,
-   PageCounter,
-   BuyButton,
-   PageFullPrice,
-   DeliveryValue
+  StyledProductPage,
+  Header,
+  ProductWrapper,
+  ProductInfo,
+  ProductInfoLine,
+  PageCounter,
+  BuyButton,
+  PageFullPrice,
+  DeliveryValue
 } from './styled';
 
 const ProductPage = ({ product }) => {
-   const [quantity, setQuantity] = useState(1);
+  const [quantity, setQuantity] = useState(1);
+  
+  const {
+    title,
+    article,
+    oldPrice: baseOldPrice,
+    price: basePrice,
+    imageUrl,
+    comments = [],
+    description,
+    delivery
+  } = product;
 
-   const {
-      title,
-      article,
-      oldPrice,
-      price,
-      imageUrl,
-      comments = [],
-      description,
-      delivery
-   } = product;
+  // ПЕРЕСЧЁТ ЦЕН с учётом количества
+  const totalPrice = basePrice * quantity;
+  const totalOldPrice = baseOldPrice ? baseOldPrice * quantity : null;
 
-   const handleBuyClick = () => {
-      console.log('открытие окна оформления заказа с количеством:', quantity);
-   };
+  const handleBuyClick = () => {
+    console.log('открытие окна оформления заказа с количеством:', quantity);
+    console.log('сумма заказа:', totalPrice);
+  };
 
-   const tabs = [
-      {
-         title: "Описание",
-         content: <Description text={description} />
-      },
-      {
-         title: "Комментарии",
-         content: <Comments comments={comments} />
-      }
-   ];
+  const tabs = [
+    {
+      title: "Описание",
+      content: <Description text={description} />
+    },
+    {
+      title: "Комментарии",
+      content: <Comments comments={comments} />
+    }
+  ];
 
-   return (
-      <StyledProductPage>
-         <Header>
-            <Title as="h1">{title}</Title>
-            <Code>{article}</Code>
-         </Header>
+  return (
+    <StyledProductPage>
+      <Header>
+        <Title as="h1">{title}</Title>
+        <Code>{article}</Code>
+      </Header>
 
-         <ProductWrapper>
-            <ImageWrapper>
-               <GalleryImage src={imageUrl} alt="3D принтер" />
-            </ImageWrapper>
-            <ProductInfo>
-               <ProductInfoLine>
-                  Цена:{" "}
-                  <PageFullPrice oldPrice={oldPrice} price={price} />
-               </ProductInfoLine>
-               <ProductInfoLine>
-                  Количество:
-                  <PageCounter
-                     value={quantity}
-                     onChange={setQuantity}
-                     minValue={1} // Передаем minValue
-                  />
-               </ProductInfoLine>
-               <ProductInfoLine>
-                  <span>Доставка:</span>{" "}
-                  <DeliveryValue>{delivery}</DeliveryValue>
-               </ProductInfoLine>
-               <BuyButton size="large" onClick={handleBuyClick}>Купить</BuyButton>
-            </ProductInfo>
-         </ProductWrapper>
+      <ProductWrapper>
+        <ImageWrapper>
+          <GalleryImage src={imageUrl} alt="3D принтер" />
+        </ImageWrapper>
+        <ProductInfo>
+          <ProductInfoLine>
+            Цена:{" "}
+            <PageFullPrice 
+              oldPrice={totalOldPrice} 
+              price={totalPrice} 
+            />
+          </ProductInfoLine>
+          <ProductInfoLine>
+            Количество: 
+            <PageCounter 
+              value={quantity} 
+              onChange={setQuantity}
+              minValue={1}
+            />
+          </ProductInfoLine>
+          <ProductInfoLine>
+            <span>Доставка:</span>{" "}
+            <DeliveryValue>{delivery}</DeliveryValue>
+          </ProductInfoLine>
+          <BuyButton size="large" onClick={handleBuyClick}>Купить</BuyButton>
+        </ProductInfo>
+      </ProductWrapper>
 
-         <Tabs tabs={tabs} />
-      </StyledProductPage>
-   );
+      <Tabs tabs={tabs} />
+    </StyledProductPage>
+  );
 };
 
 export default ProductPage;
