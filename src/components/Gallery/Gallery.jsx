f241c2e0d226bc7342e363d06395be4975131b0a
Travo1ta <vladimirkobec@gmail.com>
2026-02-19 13:59:30 +0300

    A new component has been created with Swiper

---
+++ src/components/Gallery/Gallery.jsx
@@ -0,0 +1,61 @@
+import React from 'react';
+// Импортируем Swiper и его стили
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation, Pagination, Thumbs } from 'swiper/modules';
+// Стили Swiper
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/thumbs';
+
+import {
+   GalleryContainer,
+   MainSwiper,
+   ThumbsSwiper,
+   SlideImage,
+   ThumbImage
+} from './styled';
+
+const Gallery = ({ images }) => {
+   const [thumbsSwiper, setThumbsSwiper] = React.useState(null);
+
+   if (!images?.length) return null;
+
+   return (
+      <GalleryContainer>
+         {/* Главный слайдер */}
+         <MainSwiper
+            spaceBetween={10}
+            navigation={true}
+            pagination={{ clickable: true }}
+            thumbs={{ swiper: thumbsSwiper }}
+            modules={[Navigation, Pagination, Thumbs]}
+            loop={true}
+         >
+            {images.map((image, index) => (
+               <SwiperSlide key={index}>
+                  <SlideImage src={image} alt={`Product ${index + 1}`} />
+               </SwiperSlide>
+            ))}
+         </MainSwiper>
+
+         {/* Слайдер с миниатюрами (превью) */}
+         <ThumbsSwiper
+            onSwiper={setThumbsSwiper}
+            spaceBetween={10}
+            slidesPerView={4}
+            freeMode={true}
+            watchSlidesProgress={true}
+            modules={[Navigation, Thumbs]}
+         >
+            {images.map((image, index) => (
+               <SwiperSlide key={index}>
+                  <ThumbImage src={image} alt={`Thumbnail ${index + 1}`} />
+               </SwiperSlide>
+            ))}
+         </ThumbsSwiper>
+      </GalleryContainer>
+   );
+};
+
+export default Gallery;
