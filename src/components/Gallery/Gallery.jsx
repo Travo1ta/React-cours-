60654508de777d232e94786bdf93e9ca2355fee4
Travo1ta <vladimirkobec@gmail.com>
2026-02-19 14:15:36 +0300

    We use a unique key (url + index) instead of just index

--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
-// Импортируем Swiper и его стили
-import { Swiper, SwiperSlide } from 'swiper/react';
+import { SwiperSlide } from 'swiper/react';
 import { Navigation, Pagination, Thumbs } from 'swiper/modules';
+
 // Стили Swiper
 import 'swiper/css';
 import 'swiper/css/navigation';
@@ -9,53 +9,55 @@ import 'swiper/css/pagination';
 import 'swiper/css/thumbs';
 
 import {
-   GalleryContainer,
-   MainSwiper,
-   ThumbsSwiper,
-   SlideImage,
-   ThumbImage
+  GalleryContainer,
+  MainSwiper,
+  ThumbsSwiper,
+  SlideImage,
+  ThumbImage
 } from './styled';
 
 const Gallery = ({ images }) => {
-   const [thumbsSwiper, setThumbsSwiper] = React.useState(null);
+  const [thumbsSwiper, setThumbsSwiper] = React.useState(null);
 
-   if (!images?.length) return null;
+  if (!images?.length) return null;
 
-   return (
-      <GalleryContainer>
-         {/* Главный слайдер */}
-         <MainSwiper
-            spaceBetween={10}
-            navigation={true}
-            pagination={{ clickable: true }}
-            thumbs={{ swiper: thumbsSwiper }}
-            modules={[Navigation, Pagination, Thumbs]}
-            loop={true}
-         >
-            {images.map((image, index) => (
-               <SwiperSlide key={index}>
-                  <SlideImage src={image} alt={`Product ${index + 1}`} />
-               </SwiperSlide>
-            ))}
-         </MainSwiper>
+  return (
+    <GalleryContainer>
+      {/* Главный слайдер */}
+      <MainSwiper
+        spaceBetween={10}
+        navigation={true}
+        pagination={{ clickable: true }}
+        thumbs={{ swiper: thumbsSwiper }}
+        modules={[Navigation, Pagination, Thumbs]}
+        loop={true}
+      >
+        {images.map((image, index) => (
+          // ИСПРАВЛЕНО: используем уникальный ключ (url + index) вместо просто index
+          <SwiperSlide key={`slide-${image}-${index}`}>
+            <SlideImage src={image} alt={`Product ${index + 1}`} />
+          </SwiperSlide>
+        ))}
+      </MainSwiper>
 
-         {/* Слайдер с миниатюрами (превью) */}
-         <ThumbsSwiper
-            onSwiper={setThumbsSwiper}
-            spaceBetween={10}
-            slidesPerView={4}
-            freeMode={true}
-            watchSlidesProgress={true}
-            modules={[Navigation, Thumbs]}
-         >
-            {images.map((image, index) => (
-               <SwiperSlide key={index}>
-                  <ThumbImage src={image} alt={`Thumbnail ${index + 1}`} />
-               </SwiperSlide>
-            ))}
-         </ThumbsSwiper>
-      </GalleryContainer>
-   );
+      {/* Слайдер с миниатюрами (превью) */}
+      <ThumbsSwiper
+        onSwiper={setThumbsSwiper}
+        spaceBetween={10}
+        slidesPerView={4}
+        freeMode={true}
+        watchSlidesProgress={true}
+        modules={[Navigation, Thumbs]}
+      >
+        {images.map((image, index) => (
+          // ИСПРАВЛЕНО: используем уникальный ключ (url + index) вместо просто index
+          <SwiperSlide key={`thumb-${image}-${index}`}>
+            <ThumbImage src={image} alt={`Thumbnail ${index + 1}`} />
+          </SwiperSlide>
+        ))}
+      </ThumbsSwiper>
+    </GalleryContainer>
+  );
 };
 
 export default Gallery;
