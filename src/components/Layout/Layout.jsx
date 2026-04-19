5294fd3f7fdd88f0ab47d1900308e82cd1ee8c1d
Travo1ta <vladimirkobec@gmail.com>
2026-04-19 13:48:06 +0300

    Home page refactoring (new image)

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,41 +1,41 @@
 import React from 'react';
 import { NavLink, Outlet } from 'react-router-dom';
 import {
-  LayoutWrapper,
-  Header,
-  Nav,
-  NavItem,
-  Main,
-  Footer
+   LayoutWrapper,
+   Header,
+   Nav,
+   NavItem,
+   Main,
+   Footer
 } from './styled';
 
 const Layout = () => {
-  return (
-    <LayoutWrapper>
-      <Header>
-        <Nav>
-          <NavItem>
-            <NavLink to="/" end>
-              Главная
-            </NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink to="/catalog">
-              Каталог
-            </NavLink>
-          </NavItem>
-        </Nav>
-      </Header>
-      
-      <Main>
-        <Outlet />
-      </Main>
-      
-      <Footer>
-        <p>© 2024 Интернет-магазин 3D-товаров</p>
-      </Footer>
-    </LayoutWrapper>
-  );
+   return (
+      <LayoutWrapper>
+         <Header>
+            <Nav>
+               <NavItem>
+                  <NavLink to="/" end>
+                     Главная
+                  </NavLink>
+               </NavItem>
+               <NavItem>
+                  <NavLink to="/catalog">
+                     Каталог
+                  </NavLink>
+               </NavItem>
+            </Nav>
+         </Header>
+
+         <Main>
+            <Outlet />
+         </Main>
+
+         <Footer>
+            <p>© 2024 Интернет-магазин 3D-товаров</p>
+         </Footer>
+      </LayoutWrapper>
+   );
 };
 
 export default Layout;
--- src/components/MainPage/MainPage.jsx
+++ src/components/MainPage/MainPage.jsx
@@ -3,54 +3,54 @@ import Description from '../Description/Description';
 import Line from '../Line/Line';
 import Gallery from '../Gallery/Gallery';
 import {
-  MainContainer,
-  StyledTitle,
-  StyledGallery,
-  DescriptionWrapper,
-  MainText
+   MainContainer,
+   StyledTitle,
+   StyledGallery,
+   DescriptionWrapper,
+   MainText
 } from './styled';
 
 const MainPage = ({ data }) => {
-  const [showAll, setShowAll] = useState(false);
+   const [showAll, setShowAll] = useState(false);
 
-  const getDisplayText = () => {
-    if (showAll) {
-      return data.text;
-    }
-    return data.text.length > 200 
-      ? data.text.slice(0, 200) + '...' 
-      : data.text;
-  };
+   const getDisplayText = () => {
+      if (showAll) {
+         return data.text;
+      }
+      return data.text.length > 200
+         ? data.text.slice(0, 200) + '...'
+         : data.text;
+   };
 
-  return (
-    <MainContainer>
-      <StyledTitle as="h1">Магазин 3D принтеров</StyledTitle>
-      
-      {/* Используем $ пропсы */}
-      <Line $width="670px" $thickness="4px" $rounded />
-      
-      <StyledGallery>
-        <Gallery 
-          images={data.images} 
-          width={500} 
-          height={281}
-        />
-      </StyledGallery>
-      
-      <Line $width="670px" $thickness="4px" $rounded />
-      
-      <DescriptionWrapper>
-        <MainText>
-          <Description
-            text={getDisplayText()}
-            onShowMore={() => setShowAll(!showAll)}
-            isShowAllDescription={showAll}
-            hasMoreText={data.text.length > 200}
-          />
-        </MainText>
-      </DescriptionWrapper>
-    </MainContainer>
-  );
+   return (
+      <MainContainer>
+         <StyledTitle as="h1">Магазин 3D принтеров</StyledTitle>
+
+         {/* Используем $ пропсы */}
+         <Line $width="670px" $thickness="4px" $rounded />
+
+         <StyledGallery>
+            <Gallery
+               images={data.images}
+               width={500}
+               height={281}
+            />
+         </StyledGallery>
+
+         <Line $width="670px" $thickness="4px" $rounded />
+
+         <DescriptionWrapper>
+            <MainText>
+               <Description
+                  text={getDisplayText()}
+                  onShowMore={() => setShowAll(!showAll)}
+                  isShowAllDescription={showAll}
+                  hasMoreText={data.text.length > 200}
+               />
+            </MainText>
+         </DescriptionWrapper>
+      </MainContainer>
+   );
 };
 
 export default MainPage;
